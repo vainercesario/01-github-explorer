23e4323a8258fc3c6c9278ca87ec4ec890c96c99
vainercesario <c.vainer@gmail.com>
2021-12-02 18:19:01 -0300

    initial

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { RepositoryList } from './components/RepositoryList';
+
+import './styles/global.scss';
+
+export function App() {
+  //throw new Error('eita joão, deu pau')
+  return (
+      <RepositoryList />
+  )
+}
